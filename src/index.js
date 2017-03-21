da99e92d53b47d5d506ca39aa6128d332238b003
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-21 20:51:28 +0000

    Wiring up redux-devtools to the store

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducers from './reducers'
 import './stylesheets/base/index.css';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(),
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.getElementById('root')
